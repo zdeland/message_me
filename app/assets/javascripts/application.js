c922279df4d22fda66e9ded653a7daff81752dde
Zach <zrdeland@gmail.com>
2018-12-19 01:31:11 +0000

    Fixed Message close bug

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,4 +18,12 @@
 //= require_tree .
 
 
-$(document).on('turbolinks:load',function(){$('.ui.dropdown').dropdown();})
+$(document).on('turbolinks:load',function()
+{
+  $('.ui.dropdown').dropdown();
+  $('.message .close').on('click', function() {$(this).closest('.message').transition('fade');});
+})
+
+
+  
+console.log("Testing...123..."); 
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,25 @@ class SessionsController < ApplicationController
     def new
         
     end
+    
+    #login
+    def create
+       user = User.find_by(username: params[:session][:username]) 
+       if user && user.authenticate(params[:session][:password])
+           session[:user_id] = user.id
+           flash[:success] = "Welcome, "+user.username+"!"
+           redirect_to root_path
+       else
+          flash.now[:error] = "Your login information is incorrect!"
+          render 'new' 
+       end
+    end
+    
+    #logout
+    def destroy
+       session[:user_id] = nil 
+       flash[:success] = "Goodbye!"
+       redirect_to root_path
+       
+    end
 end
